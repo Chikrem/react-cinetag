1d37136f292a03d9c11b6665d70b0022ba2f2b77
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-25 18:44:12 -0300

    Favoritando Cards

--- src/contextos/Favoritos.jsx
+++ src/contextos/Favoritos.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const FavoritosContext = createContext();
 FavoritosContext.displayName = "Favoritos";
@@ -12,4 +12,34 @@ export default function FavoritosProvider({ children }) {
             {children}
         </FavoritosContext.Provider>
     )
+}
+
+export function useFavoritoContext(){
+    // Obtém os valores do contexto FavoritosContext
+    const { favorito, setFavorito } = useContext(FavoritosContext);
+    
+    // Função para adicionar ou remover um favorito
+    function adicionarFavorito(novoFavorito) {
+        // Verifica se o novo favorito já está na lista
+        const favoritoRepetido = favorito.some(item => item.id === novoFavorito.id);
+        
+        // Cria uma nova lista baseada na lista atual de favoritos
+        let novaLista = [...favorito];
+        
+        // Se o favorito não estiver repetido, adiciona à lista
+        if(!favoritoRepetido) {
+            novaLista.push(novoFavorito);
+            return setFavorito(novaLista); // Atualiza o estado com a nova lista
+        }
+        
+        // Se o favorito já estiver na lista, remove-o
+        novaLista = favorito.filter((fav) => fav.id !== novoFavorito.id);
+        return setFavorito(novaLista); // Atualiza o estado com a nova lista
+    }
+
+    // Retorna os favoritos e a função para adicionar/remover favoritos
+    return {
+        favorito,
+        adicionarFavorito
+    }
 }
