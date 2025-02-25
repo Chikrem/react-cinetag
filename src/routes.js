8e211ec73621ba60b8fa6d3a469b702fe4572362
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-25 19:18:59 -0300

    Adicionando Pagina de Player

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import Rodape from "componentes/Rodape";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Container from "componentes/Container";
 import FavoritosProvider from "contextos/Favoritos";
+import Player from "pages/Player";
 
 function AppRoutes() {
   return (
@@ -18,6 +19,7 @@ function AppRoutes() {
           <Routes>
             <Route path="/" element={<Inicio />}></Route>
             <Route path="/favoritos" element={<Favoritos />}></Route>
+            <Route path="/:id" element={<Player />}></Route>
           </Routes>
         </FavoritosProvider>
       </Container>
